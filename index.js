0c07d309cea57ed8b90b41c5aa68363a6d75cf18
Andrew Carpenter <andrew@audioinhd.com>
2019-06-08 17:17:14 -0400

    Update go-versions

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
 'use strict'
 
-// Setup
-const goVersions = require('go-versions')
-
-// Exports
-module.exports = goLatest
-
-// Main
-function goLatest () {
-  return new Promise((resolve, reject) => {
-    goVersions().then((vl) => resolve(vl[0])).catch(reject)
-  })
-}
+module.exports = () => require('go-versions')().then((vl) => vl[0])
